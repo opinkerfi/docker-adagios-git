fb38b048b8712b930f04e15cf61426f7c9ba5928
Gardar Thorsteinsson <gardart@gmail.com>
2017-12-07 10:06:32 +0000

    converted from naemon to nagios4

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 ADAGIOS_HOST=${ADAGIOS_HOST:-localhost}
 ADAGIOS_USER=${ADAGIOS_USER:-thrukadmin}
-ADAGIOS_PASS=${ADAGIOS_PASS:-P@ssw0rd}
+ADAGIOS_PASS=${ADAGIOS_PASS:-thrukadmin}
 GIT_REPO=${GIT_REPO:-True}
 
 # Set password if htpasswd file does not exist yet
@@ -14,19 +14,19 @@ then
 	ln -s /etc/thruk/htpasswd /etc/nagios/passwd
 fi
 
-# Init git repo at /etc/naemon/
-if [[ "$GIT_REPO" = "true" && ! -d /etc/naemon/.git ]]
+# Init git repo at /etc/nagios/
+if [[ "$GIT_REPO" = "true" && ! -d /etc/nagios/.git ]]
 then
-    cd /etc/naemon
+    cd /etc/nagios
     echo "passwd" > .gitignore
     git init
     git add .
     git commit -m "Initial commit"
-    chown -R naemon /etc/naemon/.git
+    chown -R nagios /etc/nagios/.git
 fi
 
 # Create necessary logfile structure
-#touch /var/log/nagios/nagios.log
+##touch /var/log/nagios/nagios.log
 #for dir in /var/log/nagios/{archives,spool/checkresults}
 #do
 #    if [[ ! -d "$dir" ]]
@@ -35,11 +35,10 @@ fi
 #    fi
 #done
 
-# Fix permissions for naemon and pnp4nagios
-chown -R naemon:naemon /etc/naemon /etc/adagios /var/lib/adagios /var/lib/pnp4nagios /var/log/pnp4nagios /var/spool/pnp4nagios /etc/pnp4nagios/process_perfdata.cfg /var/log/okconfig
+# Fix permissions for nagios and pnp4nagios
+chown -R nagios:nagios /etc/nagios /etc/adagios /var/lib/adagios /var/lib/pnp4nagios /var/log/pnp4nagios /var/spool/pnp4nagios /etc/pnp4nagios/process_perfdata.cfg /var/log/okconfig
 # ACL group permissions need g+rwx
-chmod g+rwx -R /etc/naemon /etc/adagios /var/lib/adagios /var/lib/pnp4nagios /var/log/pnp4nagios /var/spool/pnp4nagios /etc/pnp4nagios/process_perfdata.cfg /var/log/okconfig
-setfacl -R -m group:naemon:rwx -m d:group:naemon:rwx /etc/naemon/ /etc/adagios /var/lib/adagios /var/lib/pnp4nagios  /var/log/pnp4nagios /var/spool/pnp4nagios /etc/pnp4nagios/process_perfdata.cfg /var/log/okconfig
+chmod g+rwx -R /etc/nagios /etc/adagios /var/lib/adagios /var/lib/pnp4nagios /var/log/pnp4nagios /var/spool/pnp4nagios /etc/pnp4nagios/process_perfdata.cfg /var/log/okconfig
 
 # Execute custom init scripts
 for script in $(ls -1 /opt/*.sh 2> /dev/null)
