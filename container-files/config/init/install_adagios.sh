c42b4f7e167bcc21aa805646478b4ec86f4158cf
Gardar Thorsteinsson <gardart@gmail.com>
2017-12-07 14:54:01 +0000

    Added debug logging for livestatus and some more tweaks

--- container-files/config/init/install_adagios.sh
+++ container-files/config/init/install_adagios.sh
@@ -1,2 +1,1 @@
-cd /opt/adagios && python setup.py install && cd /opt/pynag && python setup.py install && cp /opt/adagios/adagios/apache/adagios.conf /etc/httpd/conf.d/ && chmod -R 777 /etc/adagios
-nohup python /opt/adagios/adagios/manage.py runserver 0.0.0.0:8000 &
+cd /opt/adagios && python setup.py install && cd /opt/pynag && python setup.py install && cp /opt/adagios/adagios/apache/adagios.conf /etc/httpd/conf.d/
